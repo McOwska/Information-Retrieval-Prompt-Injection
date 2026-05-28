0b9f9578046f9d24bae686505b5fc04eb44188fb
mcowska <zuza.m.makowska@gmail.com>
2026-05-28 15:37:14 +0200

    poison in different hops during evaluation 2

--- main_eval.py
+++ main_eval.py
@@ -1,5 +1,5 @@
 from retireval.retireve_document import BM25Retriever, load_jsonl
-from evaluation.evaluator import run_evaluation_pipeline
+from evaluation.evaluator import run_evaluation_pipeline, save_results
 
 def main():
     print("Loading corpus...")
@@ -8,7 +8,15 @@ def main():
     print("Initializing BM25 Retriever...")
     retriever = BM25Retriever(corpus)
     
-    run_evaluation_pipeline(retriever, limit=5)
+    print("Loading poisoned corpus...")
+    poisoned_corpus = load_jsonl("data/processed/corpus_poisoned.jsonl")
+    
+    print("Initializing BM25 Retriever...")
+    poisoned_retriever = BM25Retriever(poisoned_corpus)
+    
+    res = run_evaluation_pipeline(poisoned_retriever, limit=20, results_path="results/evaluation_baseline_poisoned")
+    save_results(res, "asr, f1", "results/evaluation_baseline_poisoned/metrics.txt")
+
 
 if __name__ == "__main__":
     main()
